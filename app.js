5a9001f578796fed9eb8105992b58a476c1c09b1
Saloni <suparekh01@gmail.com>
2020-04-23 19:57:31 +0530

    Init function set up

--- app.js
+++ app.js
@@ -39,27 +39,10 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM =UICtrl.getDOMstrings();
+    var setupEventListeners = function() {
+        var DOM =UICtrl.getDOMstrings();
 
-
-    var ctrlAddItem = function()  {
-  
-        // 1. Get the field input  data
-        var input =UICtrl.getInput();
-        console.log(input);
-
-        // 2. Add the item to the budget controller
-
-        // 3. Add the item to the UI
-
-        // 4. Calculate the budget
-
-        // 5. Display the budget on the UI
-
-        
-    }
-
-    // On pressing the tick/add button
+         // On pressing the tick/add button
     document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
 
@@ -72,6 +55,43 @@ var controller = (function(budgetCtrl, UICtrl) {
             ctrlAddItem();
         }
     });
+
+    };
+
+    
+
+
+    var ctrlAddItem = function()  {
+  
+        // 1. Get the field input  data
+        var input =UICtrl.getInput();
+        
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+        
+    };
+
+
+    return {
+        init: function(){
+            console.log('Application has started');
+            setupEventListeners();
+        }
+    };
+
+
+
+   
   
 
 })(budgetController, UIController);
+
+
+controller.init();
