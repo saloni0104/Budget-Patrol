1324a874d66df231b3012fec81b15ca3511f1c0c
Saloni <suparekh01@gmail.com>
2020-04-24 20:32:44 +0530

    Updated percentage calculation

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ var budgetController = (function () {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
+    };
+
+    
+    Expense.prototype.calcPercentage = function(totalIncome) {
+        if (totalIncome > 0){
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        }
+    };
+
+    Expense.prototype.getPercentages = function() {
+        return this.percentage;
     };
 
     var Income = function (id, description, value) {
@@ -118,6 +132,31 @@ var budgetController = (function () {
                 data.percentage = -1;
             }
 
+        },
+
+        calculatePercentages: function() {
+
+            /*
+            a=20
+            b=10
+            c=40
+            income = 100
+            a =20/100 = 20%
+            b =10/100 = 10%
+            c = 40/100 = 40%
+            */
+
+            data.allItems.exp.forEach(function(cur) {
+                cur.calcPercentage(data.totals.inc);
+            });
+        },
+
+        getPercentages: function() {
+
+            var allPerc = data.allItems.exp.map(function(cur) {
+                return cur.getPercentages();
+            });
+            return allPerc;
         },
 
         getBudget: function () {
@@ -297,10 +336,13 @@ var controller = (function (budgetCtrl, UICtrl) {
     var updatePercentages = function() {
 
         // 1. Calculate percentages
+        budgetCtrl.calculatePercentages();
 
         // 2. Read percentages from the budget controller
+        var percentages = budgetCtrl.getPercentages();
 
         // 3. Update the UI with the new percentages
+        console.log(percentages);
     };
 
 
